fe59ecf3999e1f8c86b24d9727be39b3128c1e1c
p5cao <p5cao@users.noreply.github.com>
2019-12-26 19:21:17 -0800

    12.26 2

---
+++ Exercise12/Assets/scripts/me.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class me : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().AddForce(
+            new Vector2(0, 5),
+            ForceMode2D.Impulse);
+    }
+
+}
