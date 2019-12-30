1e5388b901d9bff9ff1248e1331be366b9d46a47
p5cao <33706047+p5cao@users.noreply.github.com>
2019-12-29 22:28:47 -0800

    12.29 3

---
+++ Exercise15/Exercise15/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exercise15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //print out the menu options
+            Console.WriteLine("************");
+            Console.WriteLine("Menu:\n" +
+                "1 - NewGame\n"+
+                "2 - LoadGame\n"+
+                "3 - Options\n"+
+                "4 - Quit");
+            Console.WriteLine("************");
+            Console.Write("Please give your choice: ");
+            int userInput = int.Parse(Console.ReadLine());
+            //print appropriate message according to the userInput
+            switch(userInput)
+            {
+                case 1:
+                    Console.WriteLine("Starting a new game...\n");
+                    break;
+                case 2:
+                    Console.WriteLine("Loading game...\n");
+                    break;
+                case 3:
+                    Console.WriteLine("Loading Options...\n");
+                    break;
+                case 4:
+                    Console.WriteLine("Quitting game...\n");
+                    break;
+                default:
+                    Console.WriteLine("Invalid input, please give input from 1 to 4.");
+                    break;
+            }
+        }
+    }
+}
---
+++ Exercise16/Assets/scripts/Resizer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resizer : MonoBehaviour
+{
+    /// <summary>
+    /// The Resizer class (script) will shrink and grow the game object over time.
+    /// </summary>
+    /// 
+    // timer support constants
+    float TotalResizeSeconds = (float)0.1;
+    float elapsedResizeSeconds = 0;
+
+    // resizing control constants
+    float ScaleFactorPerSecond = 1;
+    // Setting this constant to 1 will make the game object double in size in the first second, 
+    // triple in size over the first 2 seconds, and so on. 
+    int scaleFactorSignMultiplier = 1;
+    // This variable to determine either grow (when it's 1) and shrink (when it's -1)
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedResizeSeconds += Time.deltaTime;
+        if (elapsedResizeSeconds >= TotalResizeSeconds)
+        {
+            Vector3 newScale = transform.localScale;
+            if (scaleFactorSignMultiplier == 1)
+            {
+                newScale.x *= 1 + ScaleFactorPerSecond;
+                newScale.y *= 1 + ScaleFactorPerSecond;
+            }
+            else if(scaleFactorSignMultiplier == -1)
+            {
+                newScale.x *= 1 / (1 + ScaleFactorPerSecond);
+                newScale.y *= 1 / (1 + ScaleFactorPerSecond);
+            }
+            transform.localScale = newScale;
+            scaleFactorSignMultiplier *= -1;
+            elapsedResizeSeconds = 0;
+        }
+    }
+}
