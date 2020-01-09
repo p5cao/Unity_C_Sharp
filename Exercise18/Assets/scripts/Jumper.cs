ccfc1cf6a9872f1f4695450c33c5c7202fe62739
p5cao <33706047+p5cao@users.noreply.github.com>
2020-01-09 00:06:46 -0800

    1.8

---
+++ Exercise18/Assets/scripts/Jumper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumper : MonoBehaviour
+{
+    /// <summary>
+    /// Changes character on left mouse button
+    /// </summary>
+
+    // need for location of new character
+    // Update is called once per frame
+    void Update()
+    {
+        // Change character on left mouse button
+        if(Input.GetMouseButtonDown(0))
+        {
+            // convert mouse position to world position
+            Vector3 position = Input.mousePosition;
+            position.z = -Camera.main.transform.position.z;
+            position = Camera.main.ScreenToWorldPoint(position);
+
+
+            // move peekaboo to mouse position
+            gameObject.transform.position = position;
+        }
+
+    }
+}
