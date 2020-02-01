d792c4b5a296de973d6fdd66eab561a629b1ca79
p5cao <33706047+p5cao@users.noreply.github.com>
2020-02-01 13:29:31 -0800

    1

---
+++ C2exercise5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace C2exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**************");
+            Console.WriteLine("1 - NewGame");
+            Console.WriteLine("2 - LoadGame");
+            Console.WriteLine("3 - Options");
+            Console.WriteLine("4 - Quit");
+            Console.WriteLine("**************");
+            //print appropriate message according to the userInput
+            while (true)
+            {
+                Console.Write("Please give your choice: ");
+                int userInput = int.Parse(Console.ReadLine());
+                switch (userInput)
+                {
+                    case 1:
+                        Console.WriteLine("Starting a new game...\n");
+                        break;
+                    case 2:
+                        Console.WriteLine("Loading game...\n");
+                        break;
+                    case 3:
+                        Console.WriteLine("Loading Options...\n");
+                        break;
+                    case 4:
+                        // Exit the Console App when pressing Enter
+                        Console.WriteLine("Quitting game...\n");
+                        Console.Write("Press <Enter> to exit... ");
+                        while (Console.ReadKey().Key == ConsoleKey.Enter) {
+                            Environment.Exit(0);
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input, please give input from 1 to 4.");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Course2 Exercise1/Exercise1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleCards;
+
+namespace Exercise1
+{
+    /// <summary>
+    /// Exercise 1 solution
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Practices using arrays
+        /// </summary>
+        /// <param name="args">command-line arguments</param>
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+            Card[] Cards = new Card[5];
+            deck.Shuffle();
+
+            Cards[0] = deck.TakeTopCard();
+            Cards[0].FlipOver();
+            Cards[0].Print();
+
+            Cards[1] = deck.TakeTopCard();
+            Cards[1].FlipOver();
+            Cards[0].Print();
+            Cards[1].Print();
+
+            Console.WriteLine();
+        }
+    }
+}
